55ce0f126b6032f31554b476066ca2cd2b232cb7
Benson-leepingshu <61y982@ms.mingdao.edu.tw>
2021-09-07 10:19:20 +0800

    learning varible

---
+++ intro/intro.py
@@ -0,0 +1,15 @@
+print("Hello World")
+name = "Benson"
+print(f"Hello {name}")
+age = 16
+print(f"Hello {age}")
+
+if age < 18:
+    print("You are a minor")
+elif age < 21:
+    print("You can not drink alcohol in the US")
+else:
+    print("You can do anything")
+
+if __name__ == "__main__":
+    print("****")
